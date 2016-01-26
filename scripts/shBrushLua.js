d24ee4af3c676038b152c57a2d84378f9a3ef22f
tumbling-dice <inujini.dev@gmail.com>
2016-01-27 01:36:42 +0900

    :sushi: shBrushLua.jsが古すぎたので修正

--- scripts/shBrushLua.js
+++ scripts/shBrushLua.js
@@ -1,55 +1,53 @@
-/*
- *
- * shBrushLua.js (v. 0.1)
- *
- * https://github.com/eljeko/shBrushLua
- *
- * by eljeko
- *
- * Twitter: http://twitter.com/eljeko
- * Linkedin: http://www.linkedin.com/in/linguerri
- *
- * Lua brush for SyntaxHighlighter: http://alexgorbatchev.com/SyntaxHighlighter/
- * 
- * original version by bear.mini (available at: http://www.undermyhat.org/blog/2009/09/list-of-brushes-syntaxhighligher)
- *
- * License under the terms of either the MIT License or the GNU General Public License (GPL) Version 3.
- * 
- */
+;(function()
+{
+	// CommonJS
+	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+	
+	function Brush()
+	{
+		var funcs =
+			'_VERSION assert collectgarbage dofile error gcinfo loadfile loadstring print ' +
+			'tonumber tostring type unpack _ALERT _ERRORMESSAGE _INPUT _PROMPT _OUTPUT ' +
+			'_STDERR _STDIN _STDOUT call dostring foreach foreachi getn globals newtype ' +
+			'rawget rawset require sort tinsert tremove _G getfenv getmetatable ipairs ' +
+			'loadlib next pairs pcall rawegal rawget rawset require setfenv setmetatable ' +
+			'xpcall string table math coroutine io os debug ' +
+			'abs acos asin atan atan2 ceil cos deg exp floor format frexp gsub ldexp log ' +
+			'log10 max min mod rad random randomseed sin sqrt strbyte strchar strfind strlen ' +
+			'strlower strrep strsub strupper tan string.byte string.char string.dump ' +
+			'string.find string.len string.lower string.rep string.sub string.upper ' +
+			'string.format string.gfind string.gsub table.concat table.foreach ' +
+			'table.foreachi table.getn table.sort table.insert table.remove table.setn ' +
+			'math.abs math.acos math.asin math.atan math.atan2 math.ceil math.cos math.deg ' +
+			'math.exp math.floor math.frexp math.ldexp math.log math.log10 math.max math.min ' +
+			'math.mod math.pi math.rad math.random math.randomseed math.sin math.sqrt math.tan ' +
+			'openfile closefile readfrom writeto appendto remove rename flush seek tmpfile ' +
+			'tmpname read write clock date difftime execute exit getenv setlocale time ' +
+			'coroutine.create coroutine.resume coroutine.status coroutine.wrap ' +
+			'coroutine.yield io.close io.flush io.input io.lines io.open io.output io.read ' +
+			'io.tmpfile io.type io.write io.stdin io.stdout io.stderr os.clock os.date ' +
+			'os.difftime os.execute os.exit os.getenv os.remove os.rename os.setlocale ' +
+			'os.time os.tmpname';
 
-dp.sh.Brushes.Lua = function() {
-    var functions = 'math\\.\\w+ string\\.\\w+ os\\.\\w+ debug\\.\\w+ io\\.\\w+ error fopen dofile coroutine\\.\\w+ arg ipairs getmetatable loadfile longjmp loadstring rawget print seek rawset assert setmetatable tostring tonumber loadlib';
-    var keywords = 'do break end elseif else this function if local nil or not return repeat and until then while';
+		var keywords =
+			'and break do else elseif end false for function if in local nil not or repeat ' +
+			'return then true until while';
 
-    this.regexList = [
-        { regex: new RegExp(this.GetKeywords(functions), 'gm'),                 css: 'func'     },
-        { regex: new RegExp('\\b([\\d]+(\\.[\\d]+)?|0x[a-f0-9]+)\\b', 'gi'),    css: 'luanumber'}, // numbers
-        { regex: new RegExp(this.GetKeywords(keywords),  'gm'),                 css: 'keyword'  },
-        { regex: new RegExp('--\\[\\[[\\s\\S]*\\]\\]--', 'gm'),                 css: 'comments' },
-        { regex: new RegExp('--[^\\[]{2}.*$', 'gm'),                            css: 'comments' }, // one line comments
-        { regex: dp.sh.RegexLib.DoubleQuotedString,                             css: 'string'   }, // strings
-        { regex: dp.sh.RegexLib.SingleQuotedString,                             css: 'string'   }, // strings
-        { regex: new RegExp('__sub', 'gm'),                                     css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__mul', 'gm'),                                     css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__div', 'gm'),                                     css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__mod', 'gm'),                                     css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__pow', 'gm'),                                     css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__unm', 'gm'),                                     css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__concat', 'gm'),                                  css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__len', 'gm'),                                     css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__eq', 'gm'),                                      css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__lt', 'gm'),                                      css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__le', 'gm'),                                      css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__call', 'gm'),                                    css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__gc', 'gm'),                                      css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__index', 'gm'),                                   css: 'specialfields' }, //special lua field
-        { regex: new RegExp('__newindex', 'gm'),                                css: 'specialfields' }, //special lua field
-    ];
+		this.regexList = [
+			{ regex: new RegExp('--\\[\\[[\\s\\S]*\\]\\]--', 'gmi'),			css: 'comments' },
+			{ regex: new RegExp('--[^\\[]{2}.*$', 'gmi'),						css: 'comments' },
+			{ regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString,	css: 'string' },
+			{ regex: new RegExp(this.getKeywords(funcs), 'gmi'),				css: 'functions bold' },
+			{ regex: new RegExp(this.getKeywords(keywords), 'gmi'),				css: 'keyword bold' }
+		];
+	};
+	
+	Brush.prototype = new SyntaxHighlighter.Highlighter();
+	Brush.aliases = ['lua'];
 
-    this.CssClass = 'dp-lua';
-    this.Style =    '.dp-lua .luanumber { color: #440077; }' +
-                    '.dp-lua .specialfields { color: #880000; }';
-}
+	SyntaxHighlighter.brushes.Lua = Brush;
 
-dp.sh.Brushes.Lua.prototype	= new dp.sh.Highlighter();
-dp.sh.Brushes.Lua.Aliases = ['lua'];
+	// CommonJS
+	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
+
